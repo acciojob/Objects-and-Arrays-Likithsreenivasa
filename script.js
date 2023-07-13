2c8d428cd5968982a7ff5878ec8c5d53848d21ed
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-13 18:21:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const person = {
 let team=players;
 let team1=[...players]
 let cap1=[...person]
+console.log(team);    // Output: ['Player 1', 'Player 2', 'Player 3']
+console.log(team1);   // Output: ['Player 1', 'Player 2', 'Player 3']
+console.log(cap1);    // Output: { name: 'John', age: 30, city: 'New York' }
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
